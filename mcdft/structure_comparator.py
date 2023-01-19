84f84d0115c2be4168a6bd8de2084a9edc4c92cb
Musa <musa@m4llabs-mbp.myfiosgateway.com>
2023-01-19 09:13:49 -0500

    implemented nmstring

--- mcdft/structure_comparator.py
+++ mcdft/structure_comparator.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from ase.ga.utilities import get_nnmat_string
 
 
 class StructureComparator:
@@ -7,17 +7,19 @@ class StructureComparator:
         self.pair_cor_cum_diff=pair_cor_cum_diff
         self.pair_cor_max= pair_cor_max
         self.max_size = max_size
+        self.count=0
         self.structures = []
         self.sort_dists= []
     
-    def push(self, atoms, sort_dist):
+    def push(self, atoms):
         """Keep max_size number of previous structure and their sort distance
         whenever stack reached max capacity oldest"""
         if len(self.structures) >= self.max_size:
             self.structures.pop()
             self.sort_dists.pop()
-        self.structures.insert(atoms,0)
-        self.sort_dists.insert(sort_dist,0)
+        sort_dist=self.get_sorted_dist_list(atoms, mic=True)
+        self.structures=[atoms]+self.structures
+        self.sort_dists=[sort_dist]+self.sort_dists
 
     def get_sorted_dist_list(self, atoms, mic=True):
         """ Utility method used to calculate the sorted distance list
@@ -62,14 +64,22 @@ class StructureComparator:
         from latest to oldest return false as soon as it match with any structure.
         finaly return true if does not match with any other structure in the stack"""
         
-        atoms_sort_dist=self.get_sorted_dist_list(atoms, mic=True)
+        #atoms_sort_dist=self.get_sorted_dist_list(atoms, mic=True)
         
-        if len(self.structures)<self.max_size:
-            self.push(atoms,atoms_sort_dist)
+        if len(self.structures)<1:
+            
+            return True
         else:
             for structure, structure_sort_dist in zip(self.structures, self.sort_dists):
-                unique=self.compare_sorted_dist(atoms,structure,atoms_sort_dist, structure_sort_dist)
-                if unique:
+                #not_unique=self.compare_sorted_dist(atoms,structure,atoms_sort_dist, structure_sort_dist)
+                atoms_nmstring=get_nnmat_string(atoms)
+                struc_nmstring=get_nnmat_string(structure)
+                not_unique = atoms_nmstring==struc_nmstring
+                print(atoms_nmstring)
+                print(struc_nmstring)
+                if not_unique:
+                    self.count+=1
+                    print(f'match with a structure returning false. stopped {self.count} non unique structure')
                     return False
-            self.push(atoms,atoms_sort_dist)
+        print('match with no structure returning true')
         return True
